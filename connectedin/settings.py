3d6166d6f57d2ab1efac9c9aed216219f61f9a5f
Talitamota <talitamotan@gmail.com>
2019-02-04 17:20:04 -0200

    final do projeto

--- connectedin/settings.py
+++ connectedin/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'connectedin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'connectedin',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -83,3 +87,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
+LOGIN_REDIRECT_URL = '/'
+
--- perfis/views.py
+++ perfis/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from perfis.models import Perfil, Convite
 
+@login_required
 def index(request):
 	return render(request, 'index.html', {'perfis':Perfil.objects.all(),'perfil_logado':get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
 	perfil_logado = get_perfil_logado(request)
 	ja_eh_contato = perfil in perfil_logado.contatos.all()
 	return render(request, 'perfis.html', { 'perfil':perfil, 'perfil_logado':perfil_logado, 'ja_eh_contato':ja_eh_contato})
 
+@login_required
 def convidar(request, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
 	perfil_logado = get_perfil_logado(request)
 	perfil_logado.convidar(perfil_a_convidar)
 	return redirect('index')
 
+@login_required
 def aceitar(request, convite_id):
 	convite = Convite.objects.get(id=convite_id)
 	convite.aceitar()
 	return redirect('index')
-
 	
-
+@login_required
 def get_perfil_logado(request):
-	return Perfil.objects.get(id=1)
+	return request.user.perfil
